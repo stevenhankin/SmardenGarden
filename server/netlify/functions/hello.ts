c94404d6b2f9042bd6b43a420416eab800690c3f
Steven Hankin <steven.hankin@gmail.com>
2023-08-18 19:54:47 +0100

    Netlify function test

---
+++ server/netlify/functions/hello.ts
@@ -0,0 +1,16 @@
+import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import { Response } from '@netlify/functions/dist/function/response';
+
+const handler: Handler = async (
+  event: HandlerEvent,
+  context: HandlerContext
+) => {
+  // your server-side functionality
+  const response: Response = {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'Hello World' }),
+  };
+  return response;
+};
+
+export { handler };
